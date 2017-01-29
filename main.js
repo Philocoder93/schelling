cdc5e22371c5059ed516e38f60a07a3d4b1b83d7
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-01-29 13:40:30 -0500

    implemented get new board functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+//these are my object constructors
 function Red(x,y) {
   if (this instanceof Red) {
-    this.id = "started at"+x+","+y;
+    this.id = "started at "+x+","+y;
     this.color = 'red';
     this.x = x;
     this.y = y;
@@ -9,10 +10,9 @@ function Red(x,y) {
     console.log('error, this = window');
   }
 }
-
 function Blue(x,y) {
   if (this instanceof Blue) {
-    this.id = "started at"+x+","+y;
+    this.id = "started at "+x+","+y;
     this.color = 'blue';
     this.x = x;
     this.y = y;
@@ -21,12 +21,12 @@ function Blue(x,y) {
     console.log('error, this = window');
   }
 }
-
+//this is the array that the pieces will go into
 var pieces = [];
+//this is the double loop that creates the pieces and pushes them to the array
 for (x=1;x<=5;x++) {
   for (y=1;y<=5;y++) {
     var die = Math.random();
-    // console.log("die"+die);
     if (die<0.33) {
       pieces.push(new Red(x,y));
     }
@@ -37,18 +37,15 @@ for (x=1;x<=5;x++) {
     }
   }
 }
-
-$('document').ready(function(){
-  console.log('firing');
-})
-
+//this is the triple loop that creates the squares and assigns classes to them
+//based on how they line up with the array
 $('document').ready(function(){
   for (y=5;y>=1;y--) {
     for (x=1;x<=5;x++) {
       done = false;
       for (i=0;i<pieces.length;i++) {
         if ((pieces[i].x == x)&&(pieces[i].y == y)&&(pieces[i].color == 'red')) {
-          $('main').append('<div class="box '+x+' '+y+' red"><h3>this</h3></div>');
+          $('main').append('<div class="box '+x+' '+y+' red"><h3>test</h3></div>');
           done = true;
         }
         else if ((pieces[i].x == x)&&(pieces[i].y == y)&&(pieces[i].color == 'blue')) {
@@ -62,5 +59,39 @@ $('document').ready(function(){
     }
   }
 })
-
-$('document').ready()
+//new board
+$('.click').on('click',function() {
+  $('main').find('*').remove();
+  pieces =[];
+  for (x=1;x<=5;x++) {
+    for (y=1;y<=5;y++) {
+      var die = Math.random();
+      if (die<0.33) {
+        pieces.push(new Red(x,y));
+      }
+      else if (die<0.66) {
+        pieces.push(new Blue(x,y));
+      }
+      else {
+      }
+    }
+  }
+  for (y=5;y>=1;y--) {
+    for (x=1;x<=5;x++) {
+      done = false;
+      for (i=0;i<pieces.length;i++) {
+        if ((pieces[i].x == x)&&(pieces[i].y == y)&&(pieces[i].color == 'red')) {
+          $('main').append('<div class="box '+x+' '+y+' red"><h3>test</h3></div>');
+          done = true;
+        }
+        else if ((pieces[i].x == x)&&(pieces[i].y == y)&&(pieces[i].color == 'blue')) {
+          $('main').append('<div class="box '+x+' '+y+' blue"><h3>test</h3></div>');
+          done = true;
+        }
+      }
+      if (done == false) {
+        $('main').append('<div class="box '+x+' '+y+'"><h3>test</h3></div>');
+      }
+    }
+  }
+})
