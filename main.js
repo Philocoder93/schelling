07f75c56af8975f8bdf9c4015e94ca0a77382b0e
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-01-30 18:59:20 -0500

    flailing attempts to fix game logic

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //these are my object constructors
-function Red(x,y) {
+function Red(x, y) {
   if (this instanceof Red) {
     this.id = "started at "+x+","+y;
     this.color = 'red';
@@ -35,23 +35,24 @@ function White(x,y) {
 //this is the array that the pieces will go into
 var pieces = [];
 //this is the double loop that creates the pieces and pushes them to the array
-for (x=1;x<=33;x++) {
-  for (y=1;y<=33;y++) {
-    var die = Math.random();
-    if (die<0.33) {
-      pieces.push(new Red(x,y));
-    }
-    else if (die<0.66) {
-      pieces.push(new Blue(x,y));
-    }
-    else {
-      pieces.push(new White(x,y));
+function newPieces () {
+  for (x=1;x<=33;x++) {
+    for (y=1;y<=33;y++) {
+      var die = Math.random();
+      if (die<0.33) {
+        pieces.push(new Red(x,y));
+      }
+      else if (die<0.66) {
+        pieces.push(new Blue(x,y));
+      }
+      else {
+        pieces.push(new White(x,y));
+      }
     }
   }
-}
-//this is the triple loop that creates the squares and assigns classes to them
-//based on how they line up with the array
-$('document').ready(function(){
+};
+
+function piecesToAppend () {
   for (y=33;y>=1;y--) {
     for (x=1;x<=33;x++) {
       for (i=0;i<pieces.length;i++) {
@@ -68,40 +69,21 @@ $('document').ready(function(){
       }
     }
   }
+};
+
+
+//this is the triple loop that creates the squares and assigns classes to them
+//based on how they line up with the array
+$('document').ready(function(){
+  newPieces();
+  piecesToAppend();
 })
 //new board , aka previous work attached to a listener
 $('.click').on('click',function() {
   $('main').find('*').remove();
   pieces =[];
-  for (x=1;x<=33;x++) {
-    for (y=1;y<=33;y++) {
-      var die = Math.random();
-      if (die<0.33) {
-        pieces.push(new Red(x,y));
-      }
-      else if (die<0.66) {
-        pieces.push(new Blue(x,y));
-      }
-      else {
-        pieces.push(new White(x,y));
-      }
-    }
-  }
-  for (y=33;y>=1;y--) {
-    for (x=1;x<=33;x++) {
-      for (i=0;i<pieces.length;i++) {
-        if ((pieces[i].x == x)&&(pieces[i].y == y)&&(pieces[i].color == 'red')) {
-          $('main').append('<div class="box '+x+' '+y+' red"></div>');
-        }
-        else if ((pieces[i].x == x)&&(pieces[i].y == y)&&(pieces[i].color == 'blue')) {
-          $('main').append('<div class="box '+x+' '+y+' blue"></div>');
-        }
-        else if ((pieces[i].x == x)&&(pieces[i].y == y)&&(pieces[i].color == 'white')) {
-          $('main').append('<div class="box '+x+' '+y+' white"></div>');
-        }
-      }
-    }
-  }
+  newPieces();
+  piecesToAppend();
 })
 //this is a function that searches the board and finds every red or blue square that is bordered
 //by 3 squares of the opposite color
@@ -408,6 +390,7 @@ $('.next').on('click', function () {
     for (y=0;y<moves.length;y++) {
       if ((x!=y)&&(moves[x][0]==moves[y][0])) {
         rep++;
+        console.log('conflict');
       }
     }
   }
@@ -442,6 +425,168 @@ $('.next').on('click', function () {
 
 
 
+function arrayWithinArray(y) {
+  x = [];
+  for (i=0;i<y;i++) {
+    x.push(new Array(y));
+  }
+};
+
+//this works^^^
+
+function fillArray(x) {
+  var length = x.length;
+  for (i=0;i<length;i++) {
+    for (j=0;j<length;j++) {
+      var die = Math.random();
+      if (die<0.33) {
+        x[i][j] = "red";
+      }
+      else if (die<0.66) {
+        x[i][j] = "blue";
+      }
+      else {
+        x[i][j] = "white";
+      }
+    }
+  }
+};
+
+//this works too^^^
+
+function arrayToDiv(x) {
+  var length = x.length;
+  for (i=0;i<length;i++) {
+    for (j=0;j<length;j++) {
+      if (x[i][j] == 'red') {
+        $('main').append('<div class="box x:'+i+' y:'+j+' red"></div>');
+      }
+      else if (x[i][j] == 'blue') {
+        $('main').append('<div class="box x:'+i+' y:'+j+' blue"></div>');
+      }
+      else {
+        $('main').append('<div class="box x:'+i+' y:'+j+' white"></div>');
+      }
+    }
+  }
+};
+
+//this works^^^
+
+function delDiv() {
+  $('main').find('*').remove();
+};
+
+//this works^^^
+
+function checkFour(x,i,j,color) {
+  var count = 0;
+  if (x[i][j-1] == color) {
+    count = count + 1;
+  }
+  else {
+
+  }
+  if (x[i][j+1] == color) {
+    count = count + 1;
+  }
+  else {
+
+  }
+  if (x[i-1][j] == color) {
+    count = count + 1;
+  }
+  else {
+
+  }
+  if (x[i+1][j] == color) {
+    count = count + 1;
+  }
+  else {
+
+  }
+  if (count == 3) {
+    console.log(color);
+  }
+  else ()
+};
+
+//broken
+
+function checkAdjacent(x) {
+  var length = x.length;
+  for (i=0;i<length;i++) {
+    for (j=0;j<length;j++) {
+      if (x[i][j] == 'red') {
+        var count = 0;
+        if (x[i][j-1] == 'blue') {
+          count = count + 1;
+        }
+        else {
+
+        }
+        if (x[i][j+1] == 'blue') {
+          count = count + 1;
+        }
+        else {
+
+        }
+        if (x[i-1][j] == 'blue') {
+          count = count + 1;
+        }
+        else {
+
+        }
+        if (x[i+1][j] == 'blue') {
+          count = count + 1;
+        }
+        else {
+
+        }
+        if (count == 3) {
+          console.log('red');
+          count = 0;
+        }
+      }
+      else if (x[i][j] == 'blue') {
+        var count = 0;
+        var up = j+1;
+        var down = j-1;
+        var left = i-1;
+        var right = i+1;
+        if (x[i][down] == 'red') {
+          count = count + 1;
+        }
+        else {
+
+        }
+        if (x[i][up] == 'red') {
+          count = count + 1;
+        }
+        else {
+
+        }
+        if (x[left][j] == 'red') {
+          count = count + 1;
+        }
+        else {
+
+        }
+        if (x[right][j] == 'red') {
+          count = count + 1;
+        }
+        else {
+
+        }
+        if (count == 3) {
+          console.log('blue');
+          count = 0;
+        }
+      }
+    }
+  }
+};
+//broken
 
 // for (x=0;x<pieces.length;i++) {
 //   $('main').find('x:'+pieces[i].x).find('y:'+pieces[i].y).css('background',pieces[i].color);
